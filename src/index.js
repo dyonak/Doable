8ae68092b4cdb922846d24041dda6efaea694648
Dustin Yonak <dyonak@gmail.com>
2024-02-29 13:45:00 -0600

    todo base class created, need to determine project arch

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Todo } from "./todo.js";
+
+const todo1 = new Todo("test", Date.now(), Date.now(), false, "test");
+
+console.log(todo1);
---
+++ src/todo.js
@@ -0,0 +1,9 @@
+export class Todo {
+  constructor(title, createDate, dueDate, status, description) {
+    this.title = title;
+    this.createDate = createDate;
+    this.dueDate = dueDate;
+    this.status = status;
+    this.description = description;
+  }
+}
