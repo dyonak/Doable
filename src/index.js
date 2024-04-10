9df1d67f10e8919a593ead10f460a14d47b94262
Dustin Yonak <dyonak@gmail.com>
2024-04-10 11:33:18 -0500

    Removed js extention from imports to follow style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const { format, formatDistance } = require("date-fns");
 import { PubSub } from "pubsub-js";
-import { Storage } from "./Storage.js";
-import { Toast } from "./Toast.js";
+import { Storage } from "./Storage";
+import { Toast } from "./Toast";
 //import { Logger } from "./Logger.js";
-import { Todo } from "./Todo.js";
-import { List } from "./List.js";
-import { ListUi } from "./ListUI.js";
-import { ListApp } from "./ListApp.js";
+import { Todo } from "./Todo";
+import { List } from "./List";
+import { ListUi } from "./ListUI";
+import { ListApp } from "./ListApp";
 import css from "./style.css";
 
 let app = new ListApp();
